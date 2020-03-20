254d4e8544b1c5363c98350a0fe0e810cf7c4d2a
LayNath242 <laynath242@gmail.com>
2020-03-20 11:34:07 +0700

    fix

--- controller/auth.js
+++ controller/auth.js
@@ -45,10 +45,15 @@ const sendTokenResponse = (user, statusCode, res) => {
 
     const options = {
         expires: new Date(
-            Date.now() + process.env.JWT_COOKIE_EXPIR * 24 * 60 * 60 * 1000
+            Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true
     };
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true;
+    }
+
     return res
         .status(statusCode)
         .cookie('token', token, options)
